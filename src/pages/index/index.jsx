ce0a06be9116b92eb760e87effa7e57f24b916dc
chunxue.jiao <chunxue.jiao@mall-ai.com>
2020-03-27 17:50:46 +0800

    add TodoList

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,54 +1,97 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View, Button, Text } from '@tarojs/components'
-import { connect } from '@tarojs/redux'
+import Taro, { Component } from "@tarojs/taro";
+import { View, Input, Button, Text } from "@tarojs/components";
+import { connect } from "@tarojs/redux";
+import { AtButton, AtList, AtListItem } from 'taro-ui'
 
-import { add, minus, asyncAdd } from '../../actions/counter'
+import { add, minus, asyncAdd } from "../../actions/counter";
 
-import './index.scss'
+import "./index.scss";
 
-
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
-    dispatch(asyncAdd())
-  }
-}))
+@connect(
+  ({ counter }) => ({
+    counter
+  }),
+  dispatch => ({
+    add() {
+      dispatch(add());
+    },
+    dec() {
+      dispatch(minus());
+    },
+    asyncAdd() {
+      dispatch(asyncAdd());
+    }
+  })
+)
 class Index extends Component {
-    config = {
-    navigationBarTitleText: '首页'
+  constructor(props) {
+    super(props);
+    this.state = {
+      val: '',
+      todos: [
+        {
+          title: "吃饭",
+          done: false
+        },
+        {
+          title: "睡觉",
+          done: false
+        },
+        {
+          title: "coding",
+          done: false
+        }
+      ]
+    };
   }
-  super(){
-
+  inputChange = (e)=> {
+    console.log(e.target.value);
+    const newVal = e.target.value
+    this.setState(() =>({
+        val: newVal
+    }));
   }
-
-
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
+  addItem = () => {
+      const {todos, val} = this.state;
+      if (!val) return
+      this.setState(() => ({
+        todos: [...todos, {title: val, done: false}],
+        val: ''
+    }));
   }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-  render () {
+  handleChange = (e, i) => {
+    const todos = [...this.state.todos];
+    todos[i].done = e.target.value;
+    this.setState(() =>(
+        todos
+    ));
+  }
+  render() {
+    const { todos, val } = this.state;
     return (
       <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World</Text></View>
+        <Text>HelloWord</Text>
+        <Input type='text' value={val} onInput={this.inputChange} placeholder='将会获取焦点' focus />
+        {/* <Button onClick={this.addItem}>添加</Button> */}
+        <AtButton onClick={this.addItem}>添加</AtButton>
+        {todos.map((item, index) => { 
+          return <View key={index}>
+              <AtList>
+                <AtListItem
+                  title={item.title}
+                  isSwitch
+                  className={{'done':item.done}}
+                  switchIsCheck={item.done}
+                  onSwitchChange={(e) =>this.handleChange(e, index)}
+                />
+                </AtList>
+          </View>;
+        })}
+
+
       </View>
-    )
+    );
   }
 }
 
-export default Index
+export default Index;
